da3e2b5dbe4dfcc86b5abf071243002cdbcbae06
MiskovskaM <115079420+MiskovskaM@users.noreply.github.com>
2022-12-10 13:03:14 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,60 @@
+/* let number1 = 2
+let number2 = 6
 
+whaleysans.showNumber(number1)
+basic.pause (1000)
+whaleysans.showNumber(number2) */
+
+
+/* let number1 = 4
+
+while (true){
+    if (input.buttonIsPressed(Button.A)) {
+        number1 = number1 + 1
+        whaleysans.showNumber(number1)
+    } if (input.buttonIsPressed(Button.B)) {
+        number1 = number1 - 1
+        whaleysans.showNumber(number1)
+    } else {
+        whaleysans.showNumber(number1)
+    }
+}  */
+
+
+
+
+/* let number1 = 4
+
+while (true) {
+    if (input.buttonIsPressed(Button.A)) {
+        number1 += number1 
+    
+    } 
+    
+    if (input.buttonIsPressed(Button.B)) {
+        number1 -= number1
+    } 
+        whaleysans.showNumber(number1)    
+    }  
+*/
+
+
+
+
+
+
+
+/* let Number1 = 4
+let makejA = function () {
+    Number1 += 1
+    whaleysans.showNumber(Number1)
+}
+
+let makejB = function() {
+    Number1 -= 1
+    whaleysans.showNumber(Number1)
+
+}
+
+input.onButtonPressed(Button.A, makejA)
+input.onButtonPressed(Button.B, makejB) */
